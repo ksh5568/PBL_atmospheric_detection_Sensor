7a90d564b723dba802798427a86a0ac2a8179a63
ksh5568 <144317349+ksh5568@users.noreply.github.com>
2024-03-07 19:10:31 +0900

    Update PBL_Zigbee.ino

--- PBL_Zigbee/PBL_Zigbee.ino
+++ PBL_Zigbee/PBL_Zigbee.ino
@@ -5,6 +5,8 @@
 void setup()
 {
   Serial.begin(9600);
+  GLCD.Init();
+  GLCD.SelectFont(System5x7);
 }
 
 void loop()
@@ -15,52 +17,40 @@ void loop()
     Serial.println(receivedData);
    
     if (receivedData == "Pro"){
-     
-
+    GLCD.print("Propane Warning");
     }
     else if (receivedData == "Ben"){
-     
-
+     GLCD.print("Benzene Warning");
     }
     else if (receivedData == "Hex"){
-     
-
+    GLCD.print("Hexane Warning");
     }
     else if (receivedData == "H"){
-     
-
+    GLCD.print("H2 Warning");
     }
     else if (receivedData == "Alc"){
-     
-
+    GLCD.print("Alcohol Warning");
     }
     else if (receivedData == "L"){
-     
-
+    GLCD.print("LPG Warning");
     }
     else if (receivedData == "Ch"){
-     
-
+    GLCD.print("CH4 Warning");
     }
     else if (receivedData == "Co"){
-     
-
+    GLCD.print("CO Warning");
     }
     else if (receivedData == "Coo"){
-     
-
+    GLCD.print("CO2 Warning");
     }
     else if (receivedData == "Tol"){
-     
-
+    GLCD.print("Toluen Warning");
     }
     else if (receivedData == "Nh"){
-     
-
+    GLCD.print("NH4 Warning");
     }
     else if (receivedData == "Ace"){
-     
-
+    GLCD.print("Aceton Warning");
     }
   }
 }
