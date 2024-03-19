485a838ef7a463f4dbe955e28d89e6e4e5148a5b
ksh5568 <144317349+ksh5568@users.noreply.github.com>
2024-03-19 12:40:18 +0900

    Update PBL_atmospheric_detection_Sensor.ino

--- PBL_atmospheric_detection_Sensor/PBL_atmospheric_detection_Sensor.ino
+++ PBL_atmospheric_detection_Sensor/PBL_atmospheric_detection_Sensor.ino
@@ -1,5 +1,8 @@
 #include <MQUnifiedsensor.h>
 #include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
 
 //Basic Setting
 #define Board "Arduino MEGA 2560"
@@ -34,10 +37,17 @@ MQUnifiedsensor MQ5(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin_C, Type_C
 MQUnifiedsensor MQ9(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin_D, Type_D);
 MQUnifiedsensor MQ135(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin_E, Type_E);
 
+File myFile;
+
 const unsigned long interval = 2000; // 2초 (단위: 밀리초)
 unsigned long previousMillis = 0;
+String msg;
 
 void setup() {
+
+  if (!SD.begin(4)) { 
+    while (1);
+  }
  
   Serial.begin(9600);
   MQ2Sensor_Setup();
@@ -46,14 +56,21 @@ void setup() {
   MQ9Sensor_Setup();
   MQ135Sensor_Setup();
 
-  Serial.println("------------------------------------------------------------------------------------------------------------------------------------");
-  Serial.print("| Propane |  Benzene |  Hexane  |    H2    |  Alcohol |    LPG   |    CH4   |    CO    |");
-  Serial.println("    CO2   |  Toluen  |    NH4   |  Aceton  |");
-  Serial.println("------------------------------------------------------------------------------------------------------------------------------------");
+  print_setup();
+  
+  myFile = SD.open("test.txt", FILE_WRITE);
+  if (myFile) {
+    myFile.println("Coming soon :");
+    myFile.close();
+  } else {
+    Serial.println("Error opening test.txt");
+  }
 }
 
 
 void loop() {
+  myFile = SD.open("test.txt", FILE_WRITE);
+  
   //MQ2 Sensor
   MQ2Sensor_Print();
 
@@ -69,8 +86,13 @@ void loop() {
   //MQ135Sensor
   MQ135Sensor_Print();
 
+  if (myFile) {
+    myFile.println();
+    myFile.close();
+  }
+
   Serial.println();
-  delay(1000);
+  delay(2000);
 }
 
 void MQ2Sensor_Setup() {
@@ -106,10 +128,16 @@ void MQ2Sensor_Print(){
 
   float PropaneThreshold = 2000.0; 
 
-  String Pro;
-
   if(Propane > PropaneThreshold) {
-    Send_msg(Pro);
+    msg = "Pro";
+    Send_msg(msg);
+  }
+
+  if (myFile) {
+  myFile.print("|");
+  String propaneStr = String(Propane, 2);
+  printPaddedToFile(myFile, propaneStr, 8);
+  myFile.print(" | ");
   }
 }
 
@@ -154,14 +182,26 @@ void MQ3Sensor_Print(){
   float BenzeneThreshold = 100.0; // Benzene 기준치 예시
   float HexaneThreshold = 50.0; // Hexane 기준치 예시
 
-  String Ben;
-  String Hex;
-
   if(Benzene > BenzeneThreshold) {
-    Send_msg(Ben);
+    msg = "Ben";
+    Send_msg(msg);
   }
   if(Hexane > HexaneThreshold) {
-    Send_msg(Hex);
+    msg = "Hex";
+    Send_msg(msg);
+  }
+
+  if (myFile) {
+    // 벤젠 값 파일에 저장
+    myFile.print("|");
+    String BenzeneStr = String(Benzene, 2); // 소수점 둘째자리까지 문자열로 변환
+    printPaddedToFile(myFile, BenzeneStr, 8); // 파일에 8자리 폭으로 출력
+    myFile.print(" | ");
+
+    // 헥산 값 파일에 저장
+    String HexaneStr = String(Hexane, 2); // 소수점 둘째자리까지 문자열로 변환
+    printPaddedToFile(myFile, HexaneStr, 8); // 파일에 8자리 폭으로 출력
+    myFile.print(" | ");
   }
 }
 
@@ -204,14 +244,26 @@ void MQ5Sensor_Print(){
   float H2Threshold = 1000.0; // H2 기준치 예시
   float AlcoholThreshold = 400.0; // Alcohol 기준치 예시
 
-  String H;
-  String Alc;
-
   if(H2 > H2Threshold) {
-    Send_msg(H);
+    msg = "H";
+    Send_msg(msg);
   }
   if(Alcohol > AlcoholThreshold) {
-    Send_msg(Alc); 
+    msg = "Alc";
+    Send_msg(msg);
+  }
+
+  if (myFile) {
+  // 수소 값 파일에 저장
+  myFile.print("|");
+  String H2Str = String(H2, 2); // 소수점 둘째자리까지 문자열로 변환
+  printPaddedToFile(myFile, H2Str, 8); // 파일에 8자리 폭으로 출력
+  myFile.print(" | ");
+
+  // 알코올 값 파일에 저장
+  String AlcoholStr = String(Alcohol, 2); // 소수점 둘째자리까지 문자열로 변환
+  printPaddedToFile(myFile, AlcoholStr, 8); // 파일에 8자리 폭으로 출력
+  myFile.print(" | ");
   }
 }
 
@@ -263,18 +315,35 @@ void MQ9Sensor_Print(){
   float CH4Threshold = 5000.0; // CH4 기준치 예시
   float COThreshold = 30.0; // CO 기준치 예시
 
-  String L;
-  String Ch;
-  String Co;
-
   if(LPG > LPGThreshold) {
-    Send_msg(L);
+    msg = "L";
+    Send_msg(msg);
   }
   if(CH4 > CH4Threshold) {
-    Send_msg(Ch);
+    msg = "Ch";
+    Send_msg(msg);
   }
   if(CO > COThreshold) {
-    Send_msg(Co);
+    msg = "Co";
+    Send_msg(msg);
+  }
+  
+  if (myFile) {
+  // LPG 값 파일에 저장
+  myFile.print("|");
+  String LPGStr = String(LPG, 2); // 소수점 둘째자리까지 문자열로 변환
+  printPaddedToFile(myFile, LPGStr, 8); // 파일에 8자리 폭으로 출력
+  myFile.print(" | ");
+
+  // CH4 값 파일에 저장
+  String CH4Str = String(CH4, 2); // 소수점 둘째자리까지 문자열로 변환
+  printPaddedToFile(myFile, CH4Str, 8); // 파일에 8자리 폭으로 출력
+  myFile.print(" | ");
+
+  // CO 값 파일에 저장
+  String COStr = String(CO, 2); // 소수점 둘째자리까지 문자열로 변환
+  printPaddedToFile(myFile, COStr, 8); // 파일에 8자리 폭으로 출력
+  myFile.print(" | ");
   }
 }
 
@@ -335,24 +404,53 @@ void MQ135Sensor_Print(){
   float NH4Threshold = 400.0; // NH4의 기준치 예시
   float AcetonThreshold = 250.0; // Aceton의 기준치 예시
 
-  String Coo;
-  String Tol;
-  String Nh;
-  String Ace;
-
   if(CO2 > CO2Threshold) {
-    Send_msg(Coo);
+    msg = "Coo";
+    Send_msg(msg);
   }
   if(Toluen > ToluenThreshold) {
-    Send_msg(Tol);
+    msg = "Tol";
+    Send_msg(msg);
   }
   if(NH4 > NH4Threshold) {
-    Send_msg(Nh);
+    msg = "Nh";
+    Send_msg(msg);
   }
   if(Aceton > AcetonThreshold) {
-    Send_msg(Ace);
+    msg = "Ace";
+    Send_msg(msg);
   }
+
+  if (myFile) {
+  // CO2 value to file
+  myFile.print("|");
+  String CO2Str = String(CO2, 2); // Convert to String with 2 decimal places
+  printPaddedToFile(myFile, CO2Str, 8); // Print to file with padding
+  myFile.print(" | ");
+
+  // Toluen value to file
+  String ToluenStr = String(Toluen, 2);
+  printPaddedToFile(myFile, ToluenStr, 8);
+  myFile.print(" | ");
   
+  // NH4 value to file
+  String NH4Str = String(NH4, 2);
+  printPaddedToFile(myFile, NH4Str, 8);
+  myFile.print(" | ");
+
+  // Aceton value to file
+  String AcetonStr = String(Aceton, 2);
+  printPaddedToFile(myFile, AcetonStr, 8);
+  myFile.print(" | ");
+  }
+}
+
+void print_setup(void){
+  Serial.println("------------------------------------------------------------------------------------------------------------------------------------");
+  Serial.print("| Propane |  Benzene |  Hexane  |    H2    |  Alcohol |    LPG   |    CH4   |    CO    |");
+  Serial.println("    CO2   |  Toluen  |    NH4   |  Aceton  |");
+  Serial.println("------------------------------------------------------------------------------------------------------------------------------------");
+
 }
 
 void printPadded(String data, int width) {
@@ -363,13 +461,20 @@ void printPadded(String data, int width) {
   Serial.print(data); // 데이터 출력
 }
 
-void Send_msg (String msg){
-  unsigned long currentMillis = millis(); 
-   
+void printPaddedToFile(File &file, String data, int width) {
+  int padding = width - data.length(); // 필요한 패딩 계산
+  for(int i = 0; i < padding; i++) {
+    file.print(" "); // 파일에 공백으로 패딩 추가
+  }
+  file.print(data); // 파일에 데이터 출력
+}
+
+void Send_msg(String msg){
+  unsigned long currentMillis = millis();  
   // 일정 시간(interval)마다 데이터 전송
   if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    
-  Serial.write(msg);
-}
+    previousMillis = currentMillis; // 이전 시간 업데이트
+
+  Serial.write(msg.c_str());
+  }   
 }
