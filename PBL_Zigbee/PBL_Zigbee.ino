31525a3e5673f776dad21d679c7375ba818d590c
ksh5568 <144317349+ksh5568@users.noreply.github.com>
2024-03-29 21:35:07 +0900

    PBL_Zigbee.ino 업데이트

--- PBL_Zigbee/PBL_Zigbee.ino
+++ PBL_Zigbee/PBL_Zigbee.ino
@@ -7,23 +7,15 @@ void setup()
   Serial.begin(9600);
   // Initialize the GLCD 
   GLCD.Init();
-
-  // Select the font for the default text area
   GLCD.SelectFont(System5x7);
 }
 
 void loop()
 {
-  GLCD.CursorTo(0, 1);
-  
-  char test_A = Serial.read();
-  if (test_A == 'A'){
-   GLCD.print("Ready!"); 
-  }
-  
   if (Serial.available())
   {
     String receivedData = Serial.readString();
     GLCD.print(receivedData + "Warning");
+    Serial.print(receivedData + "Warning")
   }
 }
